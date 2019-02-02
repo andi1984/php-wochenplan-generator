bb9cbd6e3aba50008d391a020104f269bf13353d
Andreas Sander <andreas.sander@hotmail.de>
2019-02-02 22:05:01 +0100

    Learning areas - Setup initial data structure
    Learning areas now contain arbitrary number of tasks. With this we have an initial data structure ready to start designing our PDF.
    
    Fixes #3

--- assets/src/formular.js
+++ assets/src/formular.js
@@ -1,27 +1,56 @@
+Vue.component("Task", {
+  props: ["areaId", "taskId"],
+  template: `
+    <fieldset>
+      <legend>Aufgabe</legend>
+      <label for="description">Beschreibung</label>
+      <textarea type="text" id="description" :name="'areas[' + areaId + '][tasks][' + taskId + '][description]'"></textarea>
+      <label for="category">Kategorie</label>
+      <select id="category" :name="'areas[' + areaId + '][tasks][' + taskId + '][category]'">
+        <option value="mandatory">Pflicht</option>
+        <option value="homework">Hausaufgabe</option>
+        <option value="additional">Zusatz</option>
+      </select>
+      <label for="social">Sozialform</label>
+      <select id="social" :name="'areas[' + areaId + '][tasks][' + taskId + '][social]'">
+        <option value="single">Einzelarbeit</option>
+        <option value="partner">Partnerarbeit</option>
+        <option value="group">Gruppenarbeit</option>
+      </select>
+    </fieldset>
+  `
+});
 Vue.component("MyArea", {
-  props: [],
+  props: ["area"],
   template: `
         <fieldset>
             <legend>Fach</legend>
-            <label>Name</label>
-            <input type="text" name="areas[][name]"/>
+            <select id="area-type" :name="'areas[' + area.id + '][type]'">
+              <option value="math">Mathe</option>
+              <option value="english">Englisch</option>
+              <option value="geo">Geologie</option>
+            </select>
+            <div v-show="area.tasks.length">
+              <Task v-for="task in area.tasks" :area-id="area.id" :task-id="task.id"/>
+            </div>
+            <button type="button" v-on:click="addTask">Aufgabe hinzufügen</button>
         </fieldset>
-    `
+    `,
+  methods: {
+    addTask: function() {
+      this.area.tasks.push({ id: this.area.tasks.length });
+    }
+  }
 });
 
 var app = new Vue({
   el: "#dynamic-form",
   data: {
-    areas: [
-      {
-        id: 1
-      }
-    ]
+    areas: [{ id: 0, tasks: [{ id: 0 }] }]
   },
   methods: {
     addArea: function() {
-      return this.areas.push({ id: 2 });
+      return this.areas.push({ id: this.areas.length, tasks: [{ id: 0 }] });
     }
-    },
-  
+  }
 });
