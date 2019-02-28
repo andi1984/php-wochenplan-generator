b14c234906f688962e756a8ef06f8a53fe710998
Andreas Sander <andreas.sander@hotmail.de>
2019-02-28 21:57:55 +0100

    Inital Bulma form layout.

--- assets/src/formular.js
+++ assets/src/formular.js
@@ -1,56 +1,67 @@
 Vue.component("Task", {
-  props: ["areaId", "taskId"],
-  template: `
-    <fieldset>
-      <legend>Aufgabe</legend>
-      <label for="description">Beschreibung</label>
-      <textarea type="text" id="description" :name="'areas[' + areaId + '][tasks][' + taskId + '][description]'"></textarea>
-      <label for="category">Kategorie</label>
-      <select id="category" :name="'areas[' + areaId + '][tasks][' + taskId + '][category]'">
-        <option value="mandatory">Pflicht</option>
-        <option value="homework">Hausaufgabe</option>
-        <option value="additional">Zusatz</option>
-      </select>
-      <label for="social">Sozialform</label>
-      <select id="social" :name="'areas[' + areaId + '][tasks][' + taskId + '][social]'">
-        <option value="single">Einzelarbeit</option>
-        <option value="partner">Partnerarbeit</option>
-        <option value="group">Gruppenarbeit</option>
-      </select>
+    props: ["areaId", "taskId"],
+    template: `
+    <fieldset class="field">
+      <legend class="subtitle is-4">Aufgabe</legend>
+      <label class="label" for="description">Beschreibung</label>
+      <div class="control">
+        <textarea class="textarea" type="text" id="description" :name="'areas[' + areaId + '][tasks][' + taskId + '][description]'"></textarea>
+      </div>
+      <label class="label" for="category">Kategorie</label>
+      <div class="control">
+          <select class="select" id="category" :name="'areas[' + areaId + '][tasks][' + taskId + '][category]'">
+            <option value="mandatory">Pflicht</option>
+            <option value="homework">Hausaufgabe</option>
+            <option value="additional">Zusatz</option>
+          </select>
+      </div>
+      <label class="label" for="social">Sozialform</label>
+      <div class="control">
+          <select class="select" id="social" :name="'areas[' + areaId + '][tasks][' + taskId + '][social]'">
+            <option value="single">Einzelarbeit</option>
+            <option value="partner">Partnerarbeit</option>
+            <option value="group">Gruppenarbeit</option>
+          </select>
+      </div>
+      <hr/>
     </fieldset>
   `
 });
 Vue.component("MyArea", {
-  props: ["area"],
-  template: `
-        <fieldset>
-            <legend>Fach</legend>
-            <select id="area-type" :name="'areas[' + area.id + '][type]'">
-              <option value="math">Mathe</option>
-              <option value="english">Englisch</option>
-              <option value="geo">Geologie</option>
-            </select>
-            <div v-show="area.tasks.length">
-              <Task v-for="task in area.tasks" :area-id="area.id" :task-id="task.id"/>
+    props: ["area"],
+    template: `
+        <div class="box">
+            <fieldset class="field">
+                <legend class="subtitle is-3">Fach</legend>
+                <div class="control">
+                    <select class="select" id="area-type" :name="'areas[' + area.id + '][type]'">
+                      <option value="math">Mathe</option>
+                      <option value="english">Englisch</option>
+                      <option value="geo">Geologie</option>
+                    </select>
+                </div>
+            </fieldset>
+            <Task v-for="task in area.tasks" :area-id="area.id" :task-id="task.id"/>
+            <div class="control">
+                <button class="button" type="button" v-on:click="addTask">Aufgabe hinzufügen</button>
             </div>
-            <button type="button" v-on:click="addTask">Aufgabe hinzufügen</button>
-        </fieldset>
+        </div>
     `,
-  methods: {
-    addTask: function() {
-      this.area.tasks.push({ id: this.area.tasks.length });
+    methods: {
+        addTask: function () {
+            this.area.tasks.push({id: this.area.tasks.length});
+        }
     }
-  }
 });
 
 var app = new Vue({
-  el: "#dynamic-form",
-  data: {
-    areas: [{ id: 0, tasks: [{ id: 0 }] }]
-  },
-  methods: {
-    addArea: function() {
-      return this.areas.push({ id: this.areas.length, tasks: [{ id: 0 }] });
+    el: "#dynamic-form",
+    data: {
+        areas: [{id: 0, tasks: [{id: 0}]}]
+    },
+    methods: {
+        addArea: function () {
+            return this.areas.push({id: this.areas.length, tasks: [{id: 0}]});
+        }
     }
-  }
 });
--- index.php
+++ index.php
@@ -1,27 +1,41 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Wochenplan Generator</title>
-    </head>
-    <body>
-        <h1>Wochenplan Generator</h1>
-        <form action="generate.php" method="GET">
-            <fieldset>
-                <legend>Metadaten</legend>
+<head>
+    <title>Wochenplan Generator</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css">
+    <link rel="stylesheet" href="styles/form.css">
+<body>
+<section class="hero is-warning is-bold">
+    <div class="hero-body">
+        <div class="container">
+            <h1 class="title is-1">
+                Wochenplan
+            </h1>
+            <h2 class="subtitle is-2">
+                Generator
+            </h2>
+        </div>
+    </div>
+</section>
+<form action="generate.php" method="GET">
+    <div id="dynamic-form" class="field">
+        <fieldset class="field">
+            <legend class="subtitle is-3">Metadaten</legend>
 
-                <label for="name">Name des Wochenplans</label>
+            <label class="label" for="name">Name des Wochenplans</label>
+            <div class="control">
                 <input id="name" type="text" name="name"/>
-            </fieldset>
-            <div id="dynamic-form">
-                <button type="button" v-on:click="addArea">Fach hinzufügen</button>
-                <div v-show="areas.length">
-                    <my-area v-for="area in areas" :area="area"/>
-                </div>
-                <noscript>Bitte JavaScript aktivieren!</noscript>
             </div>
-            <button>PDF erzeugen</button>
-        </form>
-        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-        <script src="./assets/src/formular.js"></script>
-    </body>
+        </fieldset>
+        <div class="field control">
+            <button class="button" type="button" v-on:click="addArea">Fach hinzufügen</button>
+        </div>
+        <my-area v-for="area in areas" :area="area"/>
+    </div>
+    <button class="button is-large">PDF erzeugen</button>
+    <noscript>Bitte JavaScript aktivieren!</noscript>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="./assets/src/formular.js"></script>
+</body>
 </html>
