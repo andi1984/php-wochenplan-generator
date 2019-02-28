f3041bebe50d4f02196246ad1b004bde0713f3fb
Andreas Sander <andreas.sander@hotmail.de>
2019-02-28 21:10:01 +0100

    Add basic labels for PDF.

--- generate.php
+++ generate.php
@@ -1,5 +1,7 @@
 <?php
 require 'vendor/autoload.php';
+require 'helpers/labels.php';
+
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
 
@@ -8,13 +10,13 @@ $dompdf = new Dompdf(array('debugLayout' => false));
 
 // instantiate Twig
 $loader = new Twig_Loader_Filesystem('./templates');
-$twig = new Twig_Environment($loader, array(
-    //'cache' => './twig_cache',
+$twig = new Twig_Environment($loader, array(//'cache' => './twig_cache',
 ));
 
 // Generate HTML through Twig (using GET params) and load it into DomPDF.
 // error_log(json_encode($_GET));
-$dompdf->loadHtml($twig->render('index.twig', $_GET));
+error_log(json_encode(array_merge($_GET, $labels)));
+$dompdf->loadHtml($twig->render('index.twig', array_merge($_GET, $labels)));
 
 // (Optional) Setup the paper size and orientation
 $dompdf->setPaper('A4', 'landscape');
---
+++ helpers/labels.php
@@ -0,0 +1,17 @@
+<?php
+$labels = array(
+    'labels' => array(
+        // Areas
+        'math' => 'Mathematik',
+        'english' => 'Englisch',
+        'geo' => 'Erdkunde',
+        // Task category
+        'mandatory' => 'Pflichtaufgabe',
+        'homework' => 'Hausaufgabe',
+        'additional' => 'Zusatzaufgabe',
+        // Task social form
+        'single' => 'Einzelarbeit',
+        'partner' => 'Partnerarbeit',
+        'group' => 'Gruppenarbeit'
+    ),
+);
